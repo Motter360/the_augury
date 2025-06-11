9f48c87ad1f17265430a544c07c00f9d8153b98f
Thomas Hammon <hammonthomas@gmail.com>
2025-06-11 11:01:10 -0500

    Added Next.js to the frontend

---
+++ frontend/the_augury/src/app/page.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./page.module.css";
+
+
+export default function Home() {
+
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+       <div>Hello world!</div>
+       </main>
+       <footer>
+      </footer>
+    </div>
+  );
+}
